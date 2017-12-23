f7415dbee440aa129259c542e3ed552ee17461c9
Okan Aslan <okanaslan.08@gmail.com>
2017-12-23 23:46:59 +0300

    Number of entry
    We get number of entry and sort topics in decreasing order

--- eksisozluk.py
+++ eksisozluk.py
@@ -3,8 +3,20 @@ import requests
 
 page = requests.get("https://eksisozluk.com")
 tree = html.fromstring(page.content)
+lis = []
 
 trendTopics = tree.xpath("//div/div/nav/ul/li/a/text()")
+countOfTrendTopics = tree.xpath("//div/div/nav/ul/li/a/small/text()")
 
-for i in trendTopics:
-    print i
+for i in range(len(trendTopics)):
+    lis.append([int(countOfTrendTopics[i]), trendTopics[i]])
+
+for q in range(len(lis)):
+    for j in range(q):
+        if lis[q-j][0] < lis[q-j-1][0]:
+            temp = lis[q-j]
+            lis[q-j] = lis[q-j-1]
+            lis[q-j-1] = temp
+
+for k in reversed(lis):
+    print str(k[0]) + (4-len(str(k[0])))*" " + k[1]
