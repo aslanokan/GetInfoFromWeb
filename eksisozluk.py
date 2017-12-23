9cd6ad2f652236e2ac6460dc319bcd17f9d8935c
Okan Aslan <okanaslan.08@gmail.com>
2017-12-24 01:15:24 +0300

    Entries from Topics
    Now user can select a topic to look what had been written about the
    topic. Used also can see writer name and date.

--- eksisozluk.py
+++ eksisozluk.py
@@ -5,6 +5,7 @@ import webbrowser
 
 
 lis = []
+lis2 = []
 topics = []
 
 def searchForTopics(link):
@@ -22,21 +23,59 @@ def searchForTopics(link):
             topics.append(trendTopics[i])
 
 
-searchForTopics("https://eksisozluk.com/basliklar/gundem?p=1")
-searchForTopics("https://eksisozluk.com/basliklar/gundem?p=2")
-searchForTopics("https://eksisozluk.com/basliklar/gundem?p=3")
+def searchForWriter(link):
+    page = requests.get(link)
+    tree = html.fromstring(page.content)
 
-for q in range(len(lis)):
-    for j in range(q):
-        if lis[q-j][0] < lis[q-j-1][0]:
-            temp = lis[q-j]
-            lis[q-j] = lis[q-j-1]
-            lis[q-j-1] = temp
+    writer = tree.xpath("//footer/div/a[@class='entry-date permalink']/text()")
+    date = tree.xpath("//footer/div/a[@class='entry-author']/text()")
+    entryNumber = tree.xpath("//footer/div/a[@class='entry-date permalink']/@href")
 
-for k in reversed(lis):
-    print str(k[0]) + (4-len(str(k[0])))*" " + k[1]
+    for i in range(len(writer)):
+        lis2.append([writer[i], date[i], "https://eksisozluk.com" + entryNumber[i]])
+
+
+def searchForEntry(link):
+    entry = ""
+    page = requests.get(link)
+    tree = html.fromstring(page.content)
+
+    text = tree.xpath("//div[@class='content']/text() | //div[@class='content']/*/text()")
+    for i in text:
+        print i
+        entry += " " + i
+
+    print entry[6:]
+
 
 def siteAdres(userInput):
     for i in lis:
         if i[1] == userInput+" ":
-            return i[2]
+            return "https://eksisozluk.com" + i[2][:-10]
+
+def sortList():
+    for q in range(len(lis)):
+        for j in range(q):
+            if lis[q-j][0] < lis[q-j-1][0]:
+                temp = lis[q-j]
+                lis[q-j] = lis[q-j-1]
+                lis[q-j-1] = temp
+
+def searchAndPrintTopics():
+    searchForTopics("https://eksisozluk.com/basliklar/gundem?p=1")
+    searchForTopics("https://eksisozluk.com/basliklar/gundem?p=2")
+    searchForTopics("https://eksisozluk.com/basliklar/gundem?p=3")
+
+    sortList()
+
+    for k in reversed(lis):
+        print str(k[0]) + (4-len(str(k[0])))*" " + k[1]
+
+
+searchAndPrintTopics()
+nextTopic = raw_input(": ")
+searchForWriter(siteAdres(nextTopic))
+
+print "\n\n\n"
+for i in lis2:
+    print i[0], i[1], "\n" ,searchForEntry(i[2])
