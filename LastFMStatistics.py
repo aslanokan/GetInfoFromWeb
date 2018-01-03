2df5115597ff6a498f2c8202d40e46b201ba2437
Okan Aslan <okanaslan.08@gmail.com>
2018-01-03 10:14:40 +0300

    Get songs and artists
    There is only one function which finds songs and their artists. It also
    counts number of listening times.

--- LastFMStatistics.py
+++ LastFMStatistics.py
@@ -2,3 +2,40 @@
 
 from lxml import html
 import requests
+
+artists = {}
+songs = {}
+
+def getSongs(link):
+    try:
+        page = requests.get(link)
+    except requests.exceptions.ConnectionError, e:
+        print e
+
+    tree = html.fromstring(page.content)
+
+    artistName = tree.xpath("//td[@class = 'chartlist-name']/span/span/a/text()")
+    songName = tree.xpath("//td[@class = 'chartlist-name']/span/a/text()")
+    for i in range(len(artistName)):
+        artist = artistName[i]
+        song = songName[i]
+        if artist not in artists:
+            artists[artist] = {"Total": 1}
+            artists[artist][song] = 1
+        else:
+            if song not in artists[artist]:
+                artists[artist][song] = 1
+            else:
+                artists[artist][song] += 1
+            artists[artist]["Total"] += 1
+
+getSongs("https://www.last.fm/user/Filojiston/library")
+for i in range(2, 10):
+    getSongs("https://www.last.fm/user/Filojiston/library" + "?page=" + str(i))
+
+print "\n\n\n"
+for i in artists:
+    print i, "(Total:", artists[i]["Total"], ")", ":"
+    for q in artists[i]:
+        if q != "Total":
+            print "\t", artists[i][q], "=>", q
