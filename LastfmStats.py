70feffe9fdf5983422cef4ffbc4d6b54d8f64b74
Okan Aslan <okanaslan.08@gmail.com>
2018-01-05 14:12:17 +0300

    Search & detailed info
    Artist and song info added, also artist and song search added. Also,
    songs will be listed according to listening times.

--- LastfmStats.py
+++ LastfmStats.py
@@ -21,8 +21,71 @@ def getInfo(link):
 
     return lis
 
-info = getInfo("https://www.last.fm/user/Filojiston/library")
-for i in info:
-    for q in i:
-        print q
-    print
+def songInfo(info):
+    songs = {}
+    for i in info:
+        artist = i[0]
+        song = i[1]
+        date = i[2]
+        if song not in songs:
+            songs[song] = [artist, [date]]
+        else:
+            songs[song][1].append(date)
+    return songs
+
+def artistInfo(info):
+    artists = {}
+    for i in info:
+        artist = i[0]
+        song = i[1]
+        date = i[2]
+        if artist not in artists:
+            artists[artist] = {song: [date]}
+        else:
+            if song not in artists[artist]:
+                artists[artist][song] = [date]
+            else:
+                artists[artist][song].append(date)
+
+    return artists
+
+def getLink(userName, pageNumber=1):
+    link ="https://www.last.fm/user/" + userName + "/library"
+    if pageNumber > 1:
+        link += "?page=" + str(pageNumber)
+    return link
+
+user = "SilencedFear"
+
+info = []
+for i in range(1, 2):
+    info += getInfo(getLink(user, i))
+
+songs = songInfo(info) #dictionary
+artists = artistInfo(info) #dictionary
+
+def printAllInfo():
+    for i in artists:
+        print i
+        for q in artists[i]:
+            print "  ", q
+            for k in artists[i][q]:
+                print "     ", k
+        print
+
+def searchForArtist(x):
+    if x in artists:
+        songsOfX = artists[x]
+        return songsOfX
+
+def searchForSong(x):
+    if x in songs:
+        song = songs[x]
+        bandName = song[0]
+        dates = song[1]
+        return bandName, dates
+
+printAllInfo()
+
+while(True):
+    print searchForSong(raw_input(": "))
