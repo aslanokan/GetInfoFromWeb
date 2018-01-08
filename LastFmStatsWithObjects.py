04f843bc1e601cdc84fb74d43370a2892027fd3a
Okan Aslan <okanaslan.08@gmail.com>
2018-01-08 18:05:52 +0300

    Artist class
    Now with artist class more information can be gained.

--- LastFmStatsWithObjects.py
+++ LastFmStatsWithObjects.py
@@ -4,13 +4,16 @@ from lxml import html
 import requests
 
 class UserListerner():
+    userNumber = 0
+
     def __init__(self, userName):
+        UserListerner.userNumber += 1
         self.information = []
         self.songs = {}
         self.artists = {}
         self.userName = userName
         self.startPage = 1
-        self.stopPage = 480
+        self.stopPage = 20
 
         for i in range(self.startPage, self.stopPage):
             self.getInformation(self.getLink(i))
@@ -122,12 +125,41 @@ class UserListerner():
                     print "\t\t", k
             print
 
-Okan = UserListerner("Filojiston")
-Lacin = UserListerner("Lacin98")
-Serkan = UserListerner("osteosit")
+class Artist():
+    def __init__(self, name):
+        self.getLink(name)
 
-while True:
-    for i in Okan.topSongs(int(raw_input(": "))):
-        print i[0]
-        for q in range(1, len(i)):
-            print "\t", i[q]
+    def getLink(self, artistName):
+        self.linkOfArtist ="https://www.last.fm/music/"
+        for i in artistName.split():
+            self.linkOfArtist += i
+            self.linkOfArtist += "+"
+        self.linkOfArtist = self.linkOfArtist[:-1]
+
+    def getGenres(self):
+        try:
+            page = requests.get(self.linkOfArtist)
+        except requests.exceptions.ConnectionError, e:
+            print e
+        tree = html.fromstring(page.content)
+        self.genres = tree.xpath("//ul[@class = 'tags-list tags-list--global']/li/a/text()")
+
+    def getWiki(self):
+        try:
+            page = requests.get(self.linkOfArtist)
+        except requests.exceptions.ConnectionError, e:
+            print e
+        tree = html.fromstring(page.content)
+        self.shortWiki = tree.xpath("//div[@class = 'wiki-content']/p/text()")[0]
+
+Okan = UserListerner("Lacin98")
+#Lacin = UserListerner("Lacin98")
+#Serkan = UserListerner("osteosit")
+
+for i in Okan.topArtist(5):
+    for q in range(1, len(i)):
+        print i[0], "times", i[q]
+        obj = Artist(i[q])
+        obj.getGenres()
+        for k in obj.genres:
+            print "\t", k
